4280f8f835635fd5ffe71c2ce2c9d96399bdd863
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-10-10 23:37:20 +0530

    checkpoint

---
+++ backend/config.js
@@ -0,0 +1,1 @@
+export const JSW_SECRET = "anubhavsecret"
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express"
+import cors from "cors"
+
 import mainRoute from "./routes/index.js"
 
 const app = express()
 
+app.use(cors());
+app.use(express.json())
 
 app.use("/api/v1", mainRoute)
+app.listen(3000)
 
 
