8d464778cb1a2f5d413f9e069c239f0fbb6e8390
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-11-24 02:16:24 +0530

    updating db.js

--- backend/db.js
+++ backend/db.js
@@ -41,4 +41,21 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to User model
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
 export const User = mongoose.model("User", userSchema);
+export const Account = mongoose.model("Account", accountSchema);
+
+
