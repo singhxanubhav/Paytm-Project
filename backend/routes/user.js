ac6bf21ac699f56cea2bf6b34779c029299a762b
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-11-26 22:17:36 +0530

    checkpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -77,3 +77,31 @@ router.put("/", authMiddleware, async (req, res) => {
     message: "Updated successfully",
   });
 });
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
