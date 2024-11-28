d9c2c880d5c09fc87aae5014fdd83ccf9c1708a8
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-11-28 11:49:04 +0530

    checkpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRouter from "./user";
+import accountRouter from "./account"
 
 export const router = express.Router();
 
 router.use("/user", userRouter);
+router.use("/account", accountRouter);
 
 
 
